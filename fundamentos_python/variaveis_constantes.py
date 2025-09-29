b827b380451264b724a96b4d608c3b3b91352f47
Ariele Ketely <arieleketely2012@gmail.com>
2025-09-29 06:57:57 -0300

    Adicionando fundamentos de Python

---
+++ fundamentos_python/variaveis_constantes.py
@@ -0,0 +1,14 @@
+nome="Ariele"
+idade = 33
+
+nome="Ketely"
+
+nome,idade = "Ariele Ketely",33
+
+print(nome,idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES= ["SP","MG","RJ"]
+
+print(BRAZILIAN_STATES)
